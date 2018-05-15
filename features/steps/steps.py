ce51037cf6d7bf21888caadfdf6e4b4c011b9715
dmitry <420600@gmail.com>
2018-05-15 13:41:44 +0500

    Add steps to input word to search field

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,4 +1,6 @@
 from behave import *
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
 
 @Given('Заходим на сайт "{url}"')
@@ -8,4 +10,11 @@ def step_impl(context, url):
 
 @Then('Находим поле для ввода информации')
 def step_impl(context):
-    context.browser.find_element_by_xpath('//input[@id=\'search\']')
+    search_field = context.browser.find_element_by_xpath('//input[@id=\'search\']')
+    if search_field:
+        context.search_field = search_field
+
+
+@step('Вводим в поле для ввода информации "{word}"')
+def step_impl(context, word):
+    context.search_field.send_keys(word, Keys.ENTER)
